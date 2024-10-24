245e6dddf387759ec2e4a51b986f546041dc6a80
Vishal Jaiswar <vishaldevloperfd@gmail.com>
2024-10-24 22:11:41 +0530

    Updates

---
+++ app.js
@@ -0,0 +1,17 @@
+
+let counter = 0
+function increment(){
+    counter = counter + 1;
+    // console.log(counter)
+    document.getElementById('counter').innerHTML = counter;
+}
+
+function decrement(){
+    counter = counter - 1;
+    document.getElementById('counter').innerHTML = counter;
+}
+
+function reset(){
+    counter = 0
+    document.getElementById('counter').innerHTML = counter;
+}
